9514f7c09cde38bd66ec61f040c04b98641fea6a
Julian Hangstörfer <jhangstoerfer@outlook.com>
2018-07-28 15:06:41 +0200

    Move chapter class out of program

---
+++ src/MarkdownConverter/Implementation/Chapter.cs
@@ -0,0 +1,28 @@
+﻿namespace MarkdownConverter
+{
+    public class Chapter
+    {
+        public string DatePoint { get; set; }
+        public string Location { get; set; }
+        public string Protagonist { get; set; }
+        public string Html { get; set; }
+
+        public string GetNavPoint(int chapter)
+        {
+            if (!string.IsNullOrEmpty(Protagonist))
+            {
+                return Protagonist;
+            }
+            if (!string.IsNullOrEmpty(Location))
+            {
+                return Location;
+            }
+            if (!string.IsNullOrEmpty(DatePoint))
+            {
+                return DatePoint;
+            }
+
+            return $"Chapter {chapter}";
+        }
+    }
+}
--- src/MarkdownConverter/Implementation/Program.cs
+++ src/MarkdownConverter/Implementation/Program.cs
@@ -105,30 +105,4 @@ namespace MarkdownConverter
             File.WriteAllBytes(path, pdf.Content());
         }
     }
-
-    public class Chapter
-    {
-        public string DatePoint { get; set; }
-        public string Location { get; set; }
-        public string Protagonist { get; set; }
-        public string Html { get; set; }
-
-        public string GetNavPoint(int chapter)
-        {
-            if (!string.IsNullOrEmpty(Protagonist))
-            {
-                return Protagonist;
-            }
-            if (!string.IsNullOrEmpty(Location))
-            {
-                return Location;
-            }
-            if (!string.IsNullOrEmpty(DatePoint))
-            {
-                return DatePoint;
-            }
-
-            return $"Chapter {chapter}";
-        }
-    }
 }
